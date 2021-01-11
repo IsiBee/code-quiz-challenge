17a8387773f09b5f5f692a3a34d43e20d33dfef1
Isabelle Bertram <isabellebertram@Isabelles-MacBook-Air.local>
2021-01-10 20:30:26 -0600

    Able to move through each question, added some questions to test with

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,12 @@
 
 var timeCounter = 60;
+var questionNumber = 0;
 var quizScore = 0;
 
+
+
 var beginQuizEl = document.querySelector("#start-button");
+var timerEl = document.querySelector("#timer");
 
 var welcomeEl = document.querySelector(".main-container");
 var questionContainer = document.querySelector(".question-container");
@@ -14,62 +18,138 @@ var answerBEl = document.querySelector("#answer-b");
 var answerCEl = document.querySelector("#answer-c");
 var answerDEl = document.querySelector("#answer-d");
 
+var resultEl = document.querySelector("#answer-result");
+
 // Creating an array of question and answer objects.
-var quizQuestions = [{q: "",a: ""},
-{q: "",a: ""},
-{q: "",a: ""},
-{q: "",a: ""},
-{q: "",a: ""},
-{q: "",a: ""},
-{q: "",a: ""},
-{q: "",a: ""},
-{q: "",a: ""},
-{q: "What color is the sky?",a: "c"}];
+var quizQuestions = [{ q: "Inside which HTML element does JavaScript live?", a: "a" },
+{ q: "Which of the following indicates a loop?", a: "d" },
+{ q: "Which key word immediately terminates a loop?", a: "b" },
+{ q: "Which key word immediately terminates a function?", a: "c" },
+{ q: "________ are required when writing an If statement?", a: "d" },
+{ q: "", a: "" },
+{ q: "", a: "" },
+{ q: "", a: "" },
+{ q: "", a: "" },
+{ q: "What color is the sky?", a: "c" }];
 
 // Creates an array of multiple choice answer objects. 
 // These are potential answers for each question listed above.
 
-var quizMultipleChoiceAnswers = [{a: "red",b: "yellow", c: "blue", d:"green"},
-{a: "",b: "", c: "", d:""},
-{a: "",b: "", c: "", d:""},
-{a: "",b: "", c: "", d:""},
-{a: "",b: "", c: "", d:""},
-{a: "",b: "", c: "", d:""},
-{a: "",b: "", c: "", d:""},
-{a: "",b: "", c: "", d:""},
-{a: "",b: "", c: "", d:""},
-{a: "red",b: "yellow", c: "blue", d:"green"}];
+var quizMultipleChoiceAnswers = [{ a: "<script>", b: "<head>", c: "<meta>", d: "<div>" },
+{ a: "for", b: "while", c: "if", d: "a and b" },
+{ a: "alert", b: "break", c: "console.log", d: "switch" },
+{ a: "end", b: "!=", c: "return", d: "var" },
+{ a: "===", b: "else if", c: "switch statements", d: "conditions" },
+{ a: "", b: "", c: "", d: "" },
+{ a: "", b: "", c: "", d: "" },
+{ a: "", b: "", c: "", d: "" },
+{ a: "", b: "", c: "", d: "" },
+{ a: "red", b: "yellow", c: "blue", d: "green" }];
 
 
-var hideContent = function(element){
-    element.setAttribute("style","display: none;");
+var hideContent = function (element) {
+    element.setAttribute("style", "display: none;");
 };
 
-var showContent = function(element){
+var showContent = function (element) {
     element.removeAttribute("style");
+};
+
+var countDown = function () {
+    showContent(timerEl);
+    var timeLeft = setInterval(function () {
+        if (timeCounter <= 0) {
+            hideContent(timerEl);
+            clearInterval(timeLeft);
+            endQuiz();
+        }
+        else {
+            timerEl.textContent = "Timer: " + timeCounter + " s";
+            timeCounter--;
+        }
+    }, 1000);
+};
+
+var clearResult = function(){
+    resultEl.textContent = "";
+};
+
+var getAnswer = function (event) {
+    event.preventDefault();
+    var userAnswer = event.target.getAttribute("answer-id");
+
+    if (userAnswer === quizQuestions[questionNumber].a) {
+        resultEl.textContent = "Correct!";
+        timeCounter += 10;
+        quizScore += 5;
+    }
+    else {
+        resultEl.textContent = "Wrong!";
+        timeCounter -= 10;
+        quizScore -= 5;
+    }
+    // Clears the result after .5 seconds so as not to confuse the user.
+    setTimeout(clearResult, 500);
+    // Increments our question number so we move onto the next question.
+    questionNumber++;
+    // If we ran out of time or questions this will end the quiz.
+    if ((timeCounter <= 0) || (questionNumber === quizQuestions.length)) {
+        endQuiz();
+    }
+    // otherwise continue with the next question and set of answers.
+    else {
+        getQuestion();
+        getMultipleChoiceAnswers();
+        answerAEl.onclick = getAnswer;
+        answerBEl.onclick = getAnswer;
+        answerCEl.onclick = getAnswer;
+        answerDEl.onclick = getAnswer;
+    }
 }
 
-var takeQuiz = function(){
-    
+var getQuestion = function () {
+    // Displays the question in the question element
+    questionEl.textContent = quizQuestions[questionNumber].q;
+};
+
+var getMultipleChoiceAnswers = function () {
+    // Displays each potential answer
+    answerAEl.textContent = "a. " + quizMultipleChoiceAnswers[questionNumber].a;
+    answerBEl.textContent = "b. " + quizMultipleChoiceAnswers[questionNumber].b;
+    answerCEl.textContent = "c. " + quizMultipleChoiceAnswers[questionNumber].c;
+    answerDEl.textContent = "d. " + quizMultipleChoiceAnswers[questionNumber].d;
+};
+
+var takeQuiz = function () {
+    // Hide welcome page elements
     hideContent(welcomeEl);
     hideContent(beginQuizEl);
+    // begin timer
+    countDown();
+    // Show quiz HTML elements
     showContent(questionContainer);
     showContent(answerContainer);
 
-    for(var i = 0; i < quizQuestions.length; i++){
-        // Display the question
-        questionEl.textContent = quizQuestions[i].q;
-        // Display the answer options
-        answerAEl.textContent = "a. " + quizMultipleChoiceAnswers[i].a;
-        answerBEl.textContent = "b. " + quizMultipleChoiceAnswers[i].b;
-        answerCEl.textContent = "c. " + quizMultipleChoiceAnswers[i].c;
-        answerDEl.textContent = "d. " + quizMultipleChoiceAnswers[i].d;
-        
-    }
+    // The first time through we need to get a question.
+    getQuestion();
+    // Get the first set of answers.
+    getMultipleChoiceAnswers();
+    // Wait for a response before jumping into the getAnswer function.
+    answerAEl.onclick = getAnswer;
+    answerBEl.onclick = getAnswer;
+    answerCEl.onclick = getAnswer;
+    answerDEl.onclick = getAnswer;
+
+};
+
+var endQuiz = function () {
+    console.log("Ended");
 };
 
 
 // Event Listeners
+hideContent(timerEl);
 hideContent(questionContainer);
 hideContent(answerContainer);
 beginQuizEl.addEventListener("click", takeQuiz);
+
